f43c09eaa79ac4c5b026b095c14e0b037e38c0e9
Abhishek <akrajput1311@gmail.com>
2024-05-02 13:59:28 +0530

    update name

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from "react";
 import Home from "../Pages/Home";
 import { getCurrentUser } from "../api/FirestoreAPI";
-import TopBar from "../components/common/TopBar";
+import TopBar from "../components/common/TopBar/index.jsx";
 
 export default function HomeLayout() {
   const [currentUser, setCurrentUser] = useState({});
