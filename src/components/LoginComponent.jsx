bf7d5d9f060d401cea1eff1b9b6212cb7a591b93
Abhishek2533 <124396625+Abhishek2533@users.noreply.github.com>
2024-05-05 22:51:04 +0530

    update linked logo

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { LoginApi } from "../api/AuthApi";
-import LinkedinLogo from "../assets/LinkedinLogo.png";
+import LinkedinLogo from "../assets/linkedinLogo.png";
 import "../Sass/LoginComponent.scss";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
