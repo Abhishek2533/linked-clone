6e20b8f850ca2e98789c0d0e4720482ff12a5dbb
Abhishek2533 <124396625+Abhishek2533@users.noreply.github.com>
2024-05-06 03:13:20 +0530

    update authapi name

--- src/components/common/ProfilePopup/index.jsx
+++ src/components/common/ProfilePopup/index.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { onLogout } from "../../../api/AuthAPI";
+import { onLogout } from "../../../api/AuthApi";
 import { getCurrentUser } from "../../../api/FirestoreAPI";
 import Button from "../Button";
 import "./index.scss";
